c7fa118ec98b29f7e2f6dc929a3e01236cb6bdba
felipeinteli <felipe.moura@sou.inteli.edu.br>
2022-10-29 12:08:54 -0300

    Entrega Exercicio de Personalização Semana 3

--- 02_AUT_EST_ENTREGA/Semana 3/ExercPersonalizacao/backend/app.js
+++ 02_AUT_EST_ENTREGA/Semana 3/ExercPersonalizacao/backend/app.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+    const express = require('express'); 
     const app = express();
 
     const hostname = '127.0.0.1';
@@ -6,12 +6,28 @@ const express = require('express');
     const sqlite3 = require('sqlite3').verbose();
     const DBPATH = 'curriculo.db'; //use o nome que você achar melhor para o banco de dados
 
+
+    app.use(express.json());
+    app.get('/usuarios', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT cod_usuario, nome, foto, cargo, sobre_mim, endereco, telefone, email FROM usuarios ORDER BY cod_usuario ASC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
     app.use(express.json());
     app.get('/formacao', (req, res) => {
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         var db = new sqlite3.Database(DBPATH); // Abre o banco
-        var sql = 'SELECT titulo, descricao, data_inicio, data_fim FROM formacao ORDER BY data_fim DESC';
+        var sql = 'SELECT cod_usuario, cod_formacao, curso, ano_inicio, ano_fim, descricao FROM formacao ORDER BY cod_usuario ASC';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
                 throw err;
@@ -26,7 +42,7 @@ const express = require('express');
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         var db = new sqlite3.Database(DBPATH); // Abre o banco
-        var sql = 'SELECT titulo, descricao, data_inicio, data_fim FROM formacao ORDER BY data_fim DESC';
+        var sql = 'SELECT cod_usuario, cod_experiencia, nome_empresa, ano_inicio, ano_fim, cargo, atividades FROM experiencia ORDER BY cod_usuario ASC';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
                 throw err;
@@ -41,7 +57,7 @@ const express = require('express');
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         var db = new sqlite3.Database(DBPATH); // Abre o banco
-        var sql = 'SELECT titulo, descricao, data_inicio, data_fim FROM formacao ORDER BY data_fim DESC';
+        var sql = 'SELECT cod_usuario, cod_realizacoes, lorem_ipsu, ano, realizacao FROM realizacoes ORDER BY cod_usuario ASC';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
                 throw err;
@@ -56,7 +72,7 @@ const express = require('express');
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         var db = new sqlite3.Database(DBPATH); // Abre o banco
-        var sql = 'SELECT titulo, descricao, data_inicio, data_fim FROM formacao ORDER BY data_fim DESC';
+        var sql = 'SELECT cod_usuario, cod_hab ilustrator, photoshop, coreldraw, dreamweaver, html/css, cod_hab FROM habilidades ORDER BY cod_usuario ASC';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
                 throw err;
@@ -71,7 +87,7 @@ const express = require('express');
         res.statusCode = 200;
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         var db = new sqlite3.Database(DBPATH); // Abre o banco
-        var sql = 'SELECT titulo, descricao, data_inicio, data_fim FROM formacao ORDER BY data_fim DESC';
+        var sql = 'SELECT cod_usuario, , cod_personalidade, loremipsu1, loremipsu2, loremipsu3, loremipsu4, loremipsu5 FROM personalidade ORDER BY cod_usuario ASC';
         db.all(sql, [],  (err, rows ) => {
             if (err) {
                 throw err;
